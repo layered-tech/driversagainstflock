3c0d36eb273e4a1f758989c007dc8ebedb7a66ae
Christopher Pfeifer <pfeifer.christopher@gmail.com>
2026-07-13 19:49:47 -0500

    fix: ensure axios is only set on window if available
    - Wrapped `window.axios` assignments in a `typeof window` check.
    - Prevented errors in non-browser environments.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-window.axios = axios;
+if (typeof window !== 'undefined') {
+    window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.withCredentials = true;
-window.axios.defaults.withXSRFToken = true;
+    window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    window.axios.defaults.withCredentials = true;
+    window.axios.defaults.withXSRFToken = true;
+}
